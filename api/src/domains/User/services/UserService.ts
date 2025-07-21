ff2b4686534b1533250670222898ae8b42dec193
arigsf <arifilhosilva235@gmail.com>
2025-07-21 15:51:39 -0300

    feat: metodo atualizar usuario

--- api/src/domains/User/services/UserService.ts
+++ api/src/domains/User/services/UserService.ts
@@ -2,9 +2,10 @@ import prisma from "../../../../config/prismaClient";
 import { User } from "@prisma/client";
 import { hash } from "bcrypt";
 import { InvalidParamError } from "../../../../errors/InvalidParamError";
+import { NotAuthorizedError } from "../../../../errors/NotAuthorizedError";
 
 class UserService {
-	async encryptPassword(password: string){
+	private async encryptPassword(password: string){
 		return await hash(password, 10);
 	}
 
@@ -22,11 +23,54 @@ class UserService {
 		user.password = await this.encryptPassword(user.password);
 
 		const newUser = await prisma.user.create({
-			data: user,
+			data: { ...user },
 		});
 
 		return newUser;
 	}
+
+	async update(id: string, body: Partial<User>, currentUser: User){
+		const existingUser = await prisma.user.findUnique({
+			where: {
+				id: id,
+			}
+		});
+
+		if (!existingUser) {
+			throw new InvalidParamError("Usuário com ID não encontrado.");
+		}
+
+
+		if (body.role && currentUser.role !== "ADMIN" || id !== currentUser.id && currentUser.role !== "ADMIN") {
+			throw new NotAuthorizedError("Você não tem permissão para editar.");
+		}
+
+		if (body.password) {
+			body.password = await this.encryptPassword(body.password);
+		}
+
+		if (body.email) {
+			const emailExists = await prisma.user.findUnique({
+				where: {
+					email: body.email,
+				},
+			});
+
+			if (emailExists) {
+				throw new InvalidParamError("Email já está em uso.");
+			}
+		}
+
+		const updatedUser = await prisma.user.update({
+			where: {
+				id: id,
+			},
+			data: { ...body }
+		});
+
+		return updatedUser;
+	}
+
 }
 
 export default new UserService();
