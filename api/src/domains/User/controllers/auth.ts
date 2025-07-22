0b3aa6e27af920e9bda0ac1612310c5e16213c4a
arigsf <arifilhosilva235@gmail.com>
2025-07-22 13:07:59 -0300

    feat: rotas para autenticacao

---
+++ api/src/domains/User/controllers/auth.ts
@@ -0,0 +1,51 @@
+import { Request, Response, NextFunction } from "express";
+import { compare } from "bcrypt";
+import prisma from "../../../../config/prismaClient";
+import { InvalidParamError } from "../../../../errors/InvalidParamError";
+import statusCodes from "../../../../utils/constants/statusCodes";
+import { NotAuthorizedError } from "../../../../errors/NotAuthorizedError";
+import { generateJWT } from "../../../../utils/functions/auth";
+
+export async function login(req: Request, res: Response, next: NextFunction) {
+	try {
+		const { email, password } = req.body;
+		const user = await prisma.user.findUnique({ where: { email } });
+
+		if(!user){
+			throw new InvalidParamError("Email e/ou senha incorretos.");
+		}
+
+		const matchingPassword = await compare(password, user.password);
+
+		if (!matchingPassword) {
+			throw new InvalidParamError("Email e/ou senha incorretos.");
+		}
+
+		if (!user.active) {
+			throw new NotAuthorizedError("Usuário desativado.");
+		}
+
+		generateJWT(user, res);
+		
+		const returnUser = {
+			id: user.id,
+			name: user.name,
+			email: user.email,
+			role: user.role,
+			active: user.active,
+		};
+		
+		res.status(statusCodes.SUCCESS).json(returnUser);
+	} catch (error) {
+		next(error);
+	}
+}
+
+export async function logout(req: Request, res: Response, next: NextFunction) {
+	try {
+		res.clearCookie("jwt");
+		res.status(statusCodes.SUCCESS).json("Logout realizado com sucesso");
+	} catch (error) {
+		next(error);
+	}
+}
